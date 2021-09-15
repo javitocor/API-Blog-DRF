d80fd114b9cae512d1d9cd3839afb2cfc4e415ac
javitocor <javitocor@gmail.com>
2021-09-15 14:35:56 +0700

    override registration to add first and last name

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -155,3 +155,7 @@ AUTHENTICATION_BACKENDS = (
  # `allauth` specific authentication methods, such as login by e-mail
  "allauth.account.auth_backends.AuthenticationBackend",
 )
+
+REST_AUTH_REGISTER_SERIALIZERS = {
+        'REGISTER_SERIALIZER': 'blog.serializers.RegisterSerializer',
+}
